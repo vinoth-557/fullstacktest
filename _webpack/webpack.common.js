406439059339e44a49283db6220eadb3bb878b6e
Vinothkumar <vinothravichandran557@gmail.com>
2020-01-15 23:42:48 +0530

    feat: created the test project

---
+++ _webpack/webpack.common.js
@@ -0,0 +1,29 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: {
+    app: './src/client/index.js',
+  },
+  output: {
+    path: path.resolve(__dirname, '../_dist'),
+    filename: '[name].bundle.js',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'My website',
+      template: path.resolve(__dirname, '../_templates/client.html')
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+    ],
+  },
+};
